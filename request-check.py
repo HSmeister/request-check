a4fc1b8ed5b216f88d8c18e9d19b46442312da96
HSmeister <germannene15@gmail.com>
2020-11-14 19:25:39 +0100

    changed screenshot and main body

--- request-check.py
+++ request-check.py
@@ -12,6 +12,7 @@ TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
 FROM_NUM = os.getenv('FROM_NUMBER')
 TO_NUM = os.getenv('TO_NUMBER')
+URL = os.getenv('URL')
 TIME_DELAY = 60
 
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
@@ -32,7 +33,7 @@ def send_sms(message):
 
 while True:
     try:
-        answer = requests.get('https://httpstat.us/404').raise_for_status()
+        answer = requests.get(URL).raise_for_status()
         logging.info(answer.status_code)
         time.sleep(TIME_DELAY)
     except:
