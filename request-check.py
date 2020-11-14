0f4bf587bce14887c680aee2ecd4415a803515a5
HSmeister <germannene15@gmail.com>
2020-11-14 18:57:19 +0100

    added main body

--- request-check.py
+++ request-check.py
@@ -1,31 +1,43 @@
 import requests
 import os
+import logging
+import time
 
 from twilio.rest import Client
 from dotenv import load_dotenv
 
 load_dotenv()
 
-#Константы
 TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-FROM_NUM = os.getenv('FROM_NUM')
-TO_NUM = os.getenv('TO_NUM')
+FROM_NUM = os.getenv('FROM_NUMBER')
+TO_NUM = os.getenv('TO_NUMBER')
 TIME_DELAY = 60
-#Константы
 
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
+logging.basicConfig(
+    filename='requesting.log',
+    level=logging.INFO
+    )
 
-answer = requests.get('https://httpstat.us/200')
-print(answer.raise_for_status())
 
 def send_sms(message):
-    client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-
     message = client.messages.create(
-        body=message,  # текст сообщения
-        from_=FROM_NUM,  # номер, который был получен
-       to=TO_NUM,  # твой номер, на который придёт sms
+        body=message,
+        from_=FROM_NUM,
+        to=TO_NUM,
        )
 
 
+while True:
+    try:
+        answer = requests.get('https://httpstat.us/404').raise_for_status()
+        logging.info(answer.status_code)
+        time.sleep(TIME_DELAY)
+    except:
+        message = f'Сервис недоступен.\
+             Повторю попытку через {TIME_DELAY} секунд'
+        logging.error(message)
+        send_sms(message)
+        time.sleep(TIME_DELAY)
